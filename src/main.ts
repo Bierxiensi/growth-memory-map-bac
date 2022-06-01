4d50254312beace370a3de69b7b1c6db7b3962a0
bierxiensi <bierxiensi@163.com>
2022-06-01 17:33:40 +0800

    添加拦截器统一接口格式

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { HttpExceptionFilter } from './core/filter/http-exception.filter';
+import { TransformInterceptor } from './core/interceptor/transform.interceptor';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -16,6 +18,8 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, swaggerOptions);
   SwaggerModule.setup('doc', app, document);
 
+  app.useGlobalFilters(new HttpExceptionFilter()); // 注册全局错误的过滤器
+  app.useGlobalInterceptors(new TransformInterceptor()); // 全局注册拦截器
   await app.listen(3000);
 }
 bootstrap();
